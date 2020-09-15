727529907b71d43812d5a4000700dd3f749d8c8d
Timon <timon.berri@gmail.com>
2020-09-15 07:45:09 +0200

    test

---
+++ Maths2Science/Maths2Science/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Maths2Science
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(838, 452);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.LightGray;
+            this.tabPage1.Controls.Add(this.btnExit);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.btnLogin);
+            this.tabPage1.Controls.Add(this.txtPassword);
+            this.tabPage1.Controls.Add(this.txtUser);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(830, 426);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Login";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(830, 426);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "About Maths2Science";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(140, 104);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 20);
+            this.txtUser.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(140, 156);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(140, 208);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(100, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username:";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password:";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(140, 262);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 351);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Maths2Science/Maths2Science/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Maths2Science
+{
+    public partial class Login : Form
+    {
+
+
+        private SqlConnection con;
+        private SqlDataAdapter adapt;
+        private SqlCommand com;
+        private DataSet ds;
+        string conStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\timon\OneDrive\Maths2Science\Maths2Science\Maths2ScienceData.mdf;Integrated Security=True";
+
+
+        private SqlDataAdapter adapt2;
+        private SqlCommand com2;
+        private DataSet ds2;
+
+        private SqlDataAdapter adapt3;
+        private SqlCommand com3;
+        private DataSet ds3;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection(conStr);
+            adapt = new SqlDataAdapter("SELECT COUNT(*) FROM LoginTable WHERE Username = '" + txtUser.Text + "' and Password = '" + txtPassword.Text + "' and SecurityClearance = '" + "admin" + "'", con);
+            DataTable dt = new DataTable();
+            ds = new DataSet();
+
+            adapt.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                Administrator a = new Administrator();
+                a.ShowDialog();
+            }
+            else
+            {
+                adapt2 = new SqlDataAdapter("SELECT COUNT(*) FROM LoginTable WHERE Username = '" + txtUser.Text + "' and Password = '" + txtPassword.Text + "' and SecurityClearance = '" + "tutor" + "'", con);
+                DataTable dt2 = new DataTable();
+                ds2 = new DataSet();
+
+                adapt2.Fill(dt2);
+                if (dt2.Rows[0][0].ToString() == "1")
+                {
+                    this.Hide();
+                    Tutors t = new Tutors();
+                    t.ShowDialog();
+                }
+                else
+                {
+                    adapt3 = new SqlDataAdapter("SELECT COUNT(*) FROM LoginTable WHERE Username = '" + txtUser.Text + "' and Password = '" + txtPassword.Text + "' and SecurityClearance = '" + "student" + "'", con);
+                    DataTable dt3 = new DataTable();
+                    ds3 = new DataSet();
+
+                    adapt3.Fill(dt3);
+                    if (dt3.Rows[0][0].ToString() == "1")
+                    {
+                        this.Hide();
+                        Students s = new Students();
+                        s.ShowDialog();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Username or Password is incorrect.", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtPassword.Clear();
+                        txtUser.Clear();
+                    }
+
+                }
+            }
+
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
+
